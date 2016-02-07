41748891ed71a6730e1f16ccc3787eb36bfec694
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-02-07 13:05:32 -0500

    Cleaned up file structure, and added CDN links

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
-var $ = jQuery.noConflict();
-    
-    $(document).ready(function() { 
-        $('#myCarousel').carousel({ interval: 7000, cycle: true });
-    }); 
+$(document).ready(function() { 
+  $('#myCarousel').carousel({ interval: 7000, cycle: true });
+ 
+  $(".fancybox").fancybox();
+      
+  $("#single_1").fancybox({
+    helpers: {
+      title : {
+        type : 'float'
+      }
+    }
+  });
+});
